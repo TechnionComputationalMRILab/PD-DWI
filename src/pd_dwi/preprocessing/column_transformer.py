469e1db053c5385bfc62a7fb98462091efef4814
Maya Gilad <ms.maya.gilad@gmail.com>
2022-06-10 19:47:29 +0300

    Use input features attribute

--- src/pd_dwi/preprocessing/column_transformer.py
+++ src/pd_dwi/preprocessing/column_transformer.py
@@ -5,9 +5,9 @@ from sklearn.compose import ColumnTransformer as ColumnTransformerBase
 class ColumnTransformer(ColumnTransformerBase):
     def fit_transform(self, X, y=None):
         array = super(ColumnTransformer, self).fit_transform(X, y)
-        return pd.DataFrame(array, index=X.index, columns=self.get_feature_names_out(list(X.columns)))
+        return pd.DataFrame(array, index=X.index, columns=self.get_feature_names_out(self.feature_names_in_))
 
     def transform(self, X):
         array = super(ColumnTransformer, self).transform(X)
 
-        return pd.DataFrame(array, index=X.index, columns=self.get_feature_names_out(list(X.columns)))
+        return pd.DataFrame(array, index=X.index, columns=self.get_feature_names_out(self.feature_names_in_))
