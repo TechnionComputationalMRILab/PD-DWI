87581a4dd95fa9a8dcfb1f5dce3137705a633f9a
Maya Gilad <ms.maya.gilad@gmail.com>
2024-03-13 21:29:18 +0200

    Fixed relative paths

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,9 +6,11 @@ from pydantic import ValidationError
 
 from pd_dwi.config.utils import read_config
 
+dirname = os.path.dirname(__file__)
+
 
 def test_from_config_valid():
-    valid_config_path = './data/valid.yaml'
+    valid_config_path = os.path.join(dirname, './data/valid.yaml')
     read_config(valid_config_path)
 
     read_config(open(valid_config_path))
@@ -22,13 +24,13 @@ def test_from_config_valid():
     ]
 )
 def test_from_config_invalid(config_name, validation_msg):
-    invalid_config_path = f'data/{config_name}.yaml'
+    invalid_config_path = os.path.join(dirname, f'./data/{config_name}.yaml')
     with pytest.raises(ValidationError) as e:
         read_config(invalid_config_path)
     assert cast(ValidationError, e.value).errors()[0]['msg'] == validation_msg
 
 
 def test_sample_configs():
-    sample_configs_folder = '../src/pd_dwi/config/samples'
+    sample_configs_folder = os.path.join(dirname, '../src/pd_dwi/config/samples')
     for filename in os.listdir(sample_configs_folder):
         read_config(os.path.join(sample_configs_folder, filename))
