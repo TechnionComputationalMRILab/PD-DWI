b470550a42cbbfe671fcc755792995b0aa8c6572
giladmaya <46792160+giladmaya@users.noreply.github.com>
2024-06-29 23:59:23 +0300

    Added batch mode (#19)

--- src/pd_dwi/scripts/adc_preprocess_command.py
+++ src/pd_dwi/scripts/adc_preprocess_command.py
@@ -1,15 +1,24 @@
+import os.path
+
 from pd_dwi.preprocessing.adc import calculate_adc
 
 
 def adc_preprocess(args):
-    dwi_folder_path = args.dwi_folder
+    input_data = args.dwi_data
     b_values = args.b
 
-    calculate_adc(dwi_folder_path, set(b_values), dwi_folder_path)
+    if os.path.isdir(input_data):
+        calculate_adc(input_data, set(b_values), input_data)
+    else:
+        with open(input_data, mode='r') as f:
+            # Skip first line
+            f.readline()
+            for dwi_folder in f.readlines():
+                calculate_adc(dwi_folder, set(b_values), dwi_folder)
 
 
 def add_adc_parser(parser) -> None:
-    parser.add_argument('-dwi_folder', type=str, required=True, help="Path for DWI acquisition folder")
+    parser.add_argument('-dwi_data', type=str, required=True, help="Path for DWI acquisition folder or input file listing DWI folders")
     parser.add_argument('-b', type=int, nargs='+', required=True, help="B-values to use for ADC calculation")
 
     parser.set_defaults(func=adc_preprocess)
